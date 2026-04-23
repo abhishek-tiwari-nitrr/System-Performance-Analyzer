61621b100bade7e83d0b5831c08efc736dd0eef2
beery4010 <abhishek.tiwari.nitrr@gmail.com>
2026-04-23 10:42:39 +0530

    feat: add NetworkMonitor service for network metrics collection

---
+++ src/services/network_monitor.py
@@ -0,0 +1,30 @@
+import psutil
+import time
+from src.services.base_service import BaseService
+from src.logger.logger import Logger
+
+logger = Logger().setup_logs()
+
+
+class NetworkMonitor(BaseService):
+    def collect(self) -> dict:
+        try:
+            logger.info("Collecting network metrics...")
+            net_start = psutil.net_io_counters()
+            time.sleep(5)
+            net_end = psutil.net_io_counters()
+            data = {
+                "upload_speed_mb": round(
+                    (net_end.bytes_sent - net_start.bytes_sent) / (1024**2), 2
+                ),
+                "download_speed_mb": round(
+                    (net_end.bytes_recv - net_start.bytes_recv) / (1024**2), 2
+                ),
+                "bytes_sent": net_end.bytes_sent,
+                "bytes_received": net_end.bytes_recv,
+            }
+            logger.info("Network metrics collected successfully.")
+            return data
+        except Exception as e:
+            logger.error(f"Error collecting network metrics: {e}")
+            return {}
