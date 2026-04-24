14423ee6cf0bfe2182494a083d60953e389aa682
beery4010 <abhishek.tiwari.nitrr@gmail.com>
2026-04-24 10:25:47 +0530

    fix: remove sensitive config from tracking

--- src/config/config.py
+++ src/config/config.py
@@ -17,9 +17,5 @@ NETWORK_MONITOR_INTERVAL = 5
 # system metrics interval: in sec
 SYSTEM_METRICS_INTERVAL = 5
 
-# admin details 
-# ADMIN_USER = "admin"
-# ADMIN_PASS_HASH = None
-
 # Max Montior Mins
 MAX_MONITOR_MINUTES = 5
